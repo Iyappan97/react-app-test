a52f383a0fba41e2d8907c0849bee9ae8ce01f60
Iyappan97 <154623253+Iyappan97@users.noreply.github.com>
2024-07-24 02:03:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> IYAPPAN's ReactApp.
+          THIS IS MY FIRST PROJECT.--- IYAPPAN's ReactApp.
         </p>
         <a
           className="App-link"
