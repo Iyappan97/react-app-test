28095d439ca383be11abd9b8dda274c679239679
Iyappan97 <154623253+Iyappan97@users.noreply.github.com>
2024-07-25 02:58:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           THIS IS MY FIRST PROJECT.--- IYAPPAN's ReactApp.
-              modified to check the flow auto deployment 2.52 AM
+              modified to check the flow auto deployment 2.58 AM
         </p>
         <a
           className="App-link"
